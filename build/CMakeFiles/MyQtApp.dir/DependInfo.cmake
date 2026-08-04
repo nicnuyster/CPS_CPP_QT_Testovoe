d6ea99c65938542f6ccdd634ca28b2c4f24a4104
nicnuyster <shirmer.alex@gmail.com>
2026-08-04 21:32:22 +0300

    CMAKE build

---
+++ build/CMakeFiles/MyQtApp.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "MyQtApp_autogen/timestamp" "custom" "MyQtApp_autogen/deps"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QGifPlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QGifPlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QGifPlugin_Import.cpp.obj.d"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QICNSPlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QICNSPlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QICNSPlugin_Import.cpp.obj.d"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QICOPlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QICOPlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QICOPlugin_Import.cpp.obj.d"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QJpegPlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QJpegPlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QJpegPlugin_Import.cpp.obj.d"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QSvgIconPlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QSvgIconPlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QSvgIconPlugin_Import.cpp.obj.d"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QSvgPlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QSvgPlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QSvgPlugin_Import.cpp.obj.d"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QTgaPlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QTgaPlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QTgaPlugin_Import.cpp.obj.d"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QTiffPlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QTiffPlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QTiffPlugin_Import.cpp.obj.d"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QWbmpPlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QWbmpPlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QWbmpPlugin_Import.cpp.obj.d"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QWebpPlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QWebpPlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QWebpPlugin_Import.cpp.obj.d"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QWindowsIntegrationPlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QWindowsIntegrationPlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Gui/Qt5Gui_QWindowsIntegrationPlugin_Import.cpp.obj.d"
+  "H:/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Widgets/Qt5Widgets_QWindowsVistaStylePlugin_Import.cpp" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Widgets/Qt5Widgets_QWindowsVistaStylePlugin_Import.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/H_/MSYS2/mingw64/qt5-static/lib/cmake/Qt5Widgets/Qt5Widgets_QWindowsVistaStylePlugin_Import.cpp.obj.d"
+  "H:/GitHub/CPS_CPP_QT_Testovoe/build/MyQtApp_autogen/mocs_compilation.cpp" "CMakeFiles/MyQtApp.dir/MyQtApp_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/MyQtApp_autogen/mocs_compilation.cpp.obj.d"
+  "H:/GitHub/CPS_CPP_QT_Testovoe/main.cpp" "CMakeFiles/MyQtApp.dir/main.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/main.cpp.obj.d"
+  "H:/GitHub/CPS_CPP_QT_Testovoe/mainwindow.cpp" "CMakeFiles/MyQtApp.dir/mainwindow.cpp.obj" "gcc" "CMakeFiles/MyQtApp.dir/mainwindow.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
