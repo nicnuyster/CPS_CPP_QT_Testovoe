41146f6ff86512a03149e3eed40e3d706e2a687f
nicnuyster <shirmer.alex@gmail.com>
2026-08-08 11:38:59 +0300

    before changes

---
+++ Obsidian_docs/БД архитектура/BD launch.md
@@ -0,0 +1,3 @@
+**START** H:\pgsql\bin\pg_ctl.exe -D H:\pgsql\data -l H:\pgsql\data\server.log start
+
+**STOP** H:\pgsql\bin\pg_ctl.exe -D H:\pgsql\data stop
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     Database database;
-
     if(database.connect())
     {
         qDebug() << "Everything is OK.";
